00e1d8cbe47384182a189a9eac8ddd4ab5d4e552
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-06-28 13:05:43 +0100

    Add sidebar data

---
+++ src/Components/SidebarData.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+export const SidebarData = [ 
+	{
+		title: "Home",
+		Icon: <i className="fa fa-home"></i>,
+		link: "/home"
+	},
+	{
+		title: "About",
+		Icon: <i className="fa fa-user"></i>,
+		link: "/about"
+	},
+	{
+		title: "Service",
+		Icon: <i className="fa fa-list"></i>,
+		link: "/service"
+	},
+	{
+		title: "Portfolio",
+		Icon: <i className="fa fa-briefcase"></i>,
+		link: "/portfolio"
+	},
+	{
+		title: "Contact",
+		Icon: <i className="fa fa-comments"></i>,
+		link: "/contact"
+	}
+]
+
