9f84abcb3a5abe3a7109e77ef323b04c4aaeb8e6
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-06-28 13:06:31 +0100

    Map sidebar data to display on the sidebars

---
+++ src/Components/Sidebar.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import "../App.css";
+import {SidebarData} from './SidebarData';
+
+const Sidebar = () => {
+	return (
+		<div className="aside">
+			<div className="logo">
+				<Link to="/"><span>P</span>ortfolio</Link>
+				{/* <a href="#"><span>A</span>tlas</a> */}
+			</div>
+			<ul className="nav">
+				{SidebarData.map((val, key) => {
+					return <li key={key} 
+					className={window.location.pathname == val.link ? "active" : ""}
+					onClick={() => {
+						window.location.pathname = val.link;
+					}}>
+						{" "}
+						{val.Icon}{val.title}
+					</li>
+				})}
+			</ul>
+		</div>
+	)
+}
+
+export default Sidebar
