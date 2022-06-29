f27e8bcea5a7459a2f0e567cd2adc31e100bc648
Kenechukwu Oguagbaka <kc101com@gmail.com>
2022-06-29 16:48:05 +0100

    Add the neccessary routes and update the styling

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
+import Sidebar from './Components/Sidebar';
+import Home from '../src/Container/Home/Home.js';
+import About from '../src/Container/About/About';
+import {
+  BrowserRouter as Router, 
+  Routes, 
+  Route
+} from 'react-router-dom';
+import Portfolio from '../src/Container/Portfolio/Portfolio';
+import Contact from '../src/Container/Contact/Contact';
 
 function App() {
   return (
-    <div className="App">
-      Start project
-    </div>
+    <Router>
+      <Sidebar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
