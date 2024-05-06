9d46041740aa173e4777eb9a62aadaa1446c91b4
keneogu <kc101com@gmail.com>
2024-05-06 17:30:15 +0100

    update Home page

--- src/Container/Home/Home.js
+++ src/Container/Home/Home.js
@@ -13,8 +13,8 @@ const Home = () => {
 							<h3 className="hello">Hello, my name is <span className="name">Kenechukwu Oguagbaka</span></h3>
 							<h3 className="my-profession">I'm a <span className="typing">Software Engineer</span></h3>
 							<p>I’m a full-stack engineer who strives to build performant web applications 
-								with a focus on user interaction and accessibility with Ruby, Ruby on Rails, Node, Express,
-								React, and JavaScript.</p>
+								with a focus on user interaction and accessibility with Ruby, Rails, Node, Express,
+								React.js/Next.js, JavaScript and TypeScript.</p>
 							<Link to="/" className="btn hire-me">Hire Me</Link>
 						</div>
 						<div className="home-img padd-15">
